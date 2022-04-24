03a31f4a20378faa1238cf26e05eea023e62518c
zschen211 <zhoushichen99@163.com>
2022-04-24 17:44:34 +0800

    fingerprint not viable after tested

--- disperse.py
+++ disperse.py
@@ -2,6 +2,7 @@ import os
 import sys
 import argparse
 from util import *
+from hashlib import sha256
 
 parser = argparse.ArgumentParser()
 parser.add_argument('inputFile', help='the input filename')
@@ -10,6 +11,7 @@ def disperse(filename):
     A = None
     fragment_matrix = None
 
+    # perform fragmentation
     with open(filename, 'rb') as f:
         # split file into segments of size m
         payload = list(f.read())
@@ -22,11 +24,14 @@ def disperse(filename):
         
         fragment_matrix = A @ segments.T
 
-    # write fragments to files
+    # write fragments to files & create fingerprint for each fragment
     for i in range(n):
-        out_filename = "./fragments/" + str(i)
-        os.makedirs(os.path.dirname(out_filename), exist_ok=True)
-        with open(out_filename, 'wb') as f:
+        frag_filename = "./fragments/" + str(i)
+        fp_filename = "./fingerprints/" + str(i)
+        os.makedirs(os.path.dirname(frag_filename), exist_ok=True)
+        os.makedirs(os.path.dirname(fp_filename), exist_ok=True)
+
+        with open(frag_filename, 'wb') as f:
             f.write(bytes(list(A[i]) + list(fragment_matrix[i])))
 
 
--- util.py
+++ util.py
@@ -1,9 +1,17 @@
 import numpy as np
+import sympy as sym
 import galois
+from cryptography.fernet import Fernet
+from sympy.polys import subresultants_qq_zz
 
 n = 4
 m = 3
+deg = 11 # degree of the irreducible polynomial
+
 GF = galois.GF(2 ** 8) # modular arithemtic uses finite field GF(2^8)
+F = Fernet(Fernet.generate_key())
+f_poly = galois.irreducible_poly(2 ** 8, deg, method='random')
+
 
 def gen_encoding_matrix(n, m):
     """Create an n x m encoding matrix.
@@ -20,7 +28,32 @@ def gen_encoding_matrix(n, m):
         A[:,j] = A[:,j] ** j
     return A
 
+def gen_fingerprint(F_coefs, fp_filename):
+    x = sym.symbols('x')
+
+    f_coefs = list(map(int, list(f_poly.coeffs)))
+    F_coefs = list(map(int, F_coefs))
+
+    size = deg + len(F_coefs)
+    
+    with open(fp_filename, 'wb') as f:
+        for i in range(size):
+            row = []
+
+            if i in range(0, deg):
+                row = f_coefs
+                row.extend((n-1-i)*[0])
+                row[:0] = [0]*i
+
+            if i in range(deg, size):
+                row = F_coefs
+                row.extend((size-1-i)*[0])
+                row[:0] = [0]*(size-len(row))
+
+            f.write(bytes(row))
 
 if __name__ == "__main__":
-    A = gen_encoding_matrix(6, 4)
-    print(A, type(A))
+    # A = gen_encoding_matrix(6, 4)
+    # print(A, type(A))
+    # gen_fingerprint(list(f_poly.coeffs))
+    pass
